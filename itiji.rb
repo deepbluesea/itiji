84051ab551e2ee8fdbf5b68ef2d263f211ef9d66
Erik Nomitch <erik@nomitch.com>
2013-05-16 01:47:03 -0700

    added Cleverbot response

--- itiji.rb
+++ itiji.rb
@@ -2,18 +2,18 @@ require 'rubygems'
 require 'cleverbot-api'
 require 'cinch'
 
-cleverbot = CleverBot.new
+@cleverbot = CleverBot.new
 
-ircbot = Cinch::Bot.new do
+@ircbot = Cinch::Bot.new do
   configure do |c|
     c.server = "irc.freenode.org"
     c.channels = ["#iit"]
     c.nick = "itiji"
   end
 
-  on :message, /test/ do |m|
-    m.reply "test"
+  on :message, /itiji/ do |m|
+    m.reply @cleverbot.think m.message
   end
 end
 
-ircbot.start
+@ircbot.start
